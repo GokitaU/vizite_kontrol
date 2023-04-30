5df9d97bf9930859513d2a18d3d1dc4575e20aaf
Ümit Kaya <110849840+umitkayapyt@users.noreply.github.com>
2023-04-30 14:39:25 +0300

    Update _fileData.py

--- _fileData.py
+++ _fileData.py
@@ -13,7 +13,10 @@ class DosyaIslem:
     def DosyaOnIslem(self):
 
         df= read_excel(self.Dosya, header=None, engine='openpyxl')
-        df = df.apply(lambda x: x.replace(r'^\s*$', NaT, regex=True) if x.dtype == "object" or "float64" or "float32" or "int" else x)
+        df = df.apply(lambda x: x.replace(r'^\s*$', NaT, regex=True) if x.dtype == "object" else x)
+        df = df.apply(lambda x: x.replace(nan, NaT, regex=True) if x.dtype == "float64" else x)
+        df = df.apply(lambda x: x.replace(nan, NaT, regex=True) if x.dtype == "float32" else x)
+        df = df.apply(lambda x: x.replace(nan, NaT, regex=True) if x.dtype == "int" else x)
         df.dropna(how='all',axis=1, inplace=True)
         df.dropna(how='all',axis=0, inplace=True)
         df=df.rename(index={j: i for i, j in enumerate(df.index)})
@@ -21,22 +24,24 @@ class DosyaIslem:
         Index = len(df.index)
         baslikkontrol = [p for p in df.values[0] if isinstance(p, float) or isinstance(p, int)] #başlıkları varsa boş liste
 
-        if baslikkontrol==[]:
+        if baslikkontrol==[]: #başlık var ise
             df = df.iloc[1:Index]
             df=df.rename(index={j: i for i, j in enumerate(df.index)})
-            
-            lokasyon = df[1].values
-            kullaniciKodu = df[2].apply(int).values
-            isyeriKodu = df[3].apply(int).values
-            sifre = df[4].astype(str, errors = 'ignore')
+
+            df.columns = [0,1,2,3]
+            lokasyon = df[0].values
+            kullaniciKodu = df[1].apply(int).values
+            isyeriKodu = df[2].apply(int).values
+            sifre = df[3].astype(str, errors = 'ignore')
 
             return kullaniciKodu, isyeriKodu, sifre, lokasyon, df
         
         else:
-            lokasyon = df[1].values
-            kullaniciKodu = df[2].apply(int).values
-            isyeriKodu = df[3].apply(int).values
-            sifre = df[4].astype(str, errors = 'ignore')
+            df.columns = [0,1,2,3]
+            lokasyon = df[0].values
+            kullaniciKodu = df[1].apply(int).values
+            isyeriKodu = df[2].apply(int).values
+            sifre = df[3].astype(str, errors = 'ignore')
             
             return kullaniciKodu, isyeriKodu, sifre, lokasyon, df
 

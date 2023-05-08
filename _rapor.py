ea0a0c05d3745a71d94e033dc525a66c544ebbaf
Ümit Kaya <110849840+umitkayapyt@users.noreply.github.com>
2023-05-08 20:45:13 +0300

    _rapor.py

--- _rapor.py
+++ _rapor.py
@@ -331,7 +331,7 @@ class RaporKontrol(QMainWindow):
 
         #sleep(0.1)
         
-        n = strftime("%d.%m.%Y")
+        buGun = strftime("%d.%m.%Y")
         TarihBitisXpath = '/html/body/table[2]/tbody/tr/td[2]/form/table/tbody/tr[2]/td[2]/input'
         TarihBitisXpathInput = self.driver.find_element(By.XPATH, TarihBitisXpath)
 
